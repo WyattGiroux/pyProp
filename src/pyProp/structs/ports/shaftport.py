90fec209e7ad1b45af64c3af0fc744384fcd956d
Wyatt Giroux <girouxw@mit.edu>
2025-02-04 14:58:59 -0500

    added a comment to shaftport

--- src/pyProp/structs/ports/shaftport.py
+++ src/pyProp/structs/ports/shaftport.py
@@ -1,6 +1,9 @@
 from .port import Port
 
 class ShaftPort(Port):
+    '''Port used for rigid shaft connections
+    
+    '''
     def __init__(self, name):
         super().__init__(name)
         self.Nmech = 0
